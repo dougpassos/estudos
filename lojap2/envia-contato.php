1d5f700b52446d3f559f4a82bc8b99d9e26533b5
Douglas Passos <douglas.p.doug@gmail.com>
2015-10-13 18:48:34 -0300

    Php email

--- lojap2/envia-contato.php
+++ lojap2/envia-contato.php
@@ -1,9 +1,33 @@
 <?php
+session_start();
+
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $mensage = $_POST['mensagem'];
-echo $nome;
-echo $email;
-echo $mensage;
+
+require_once("PHPMailerAutoload.php");
+
+$mail = new PHPMailer();
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+$mail->SMTPSecure = 'tls';
+$mail->SMTPAuth = true;
+$mail->Username = "douglas.p.doug@gmail.com";
+$mail->Password = "123456";
+$mail->setFrom("douglas.p.doug@gmail.com", "Alura Curso PHP e MySQL");
+$mail->addAddress("alura.php.e.mysql@gmail.com");
+$mail->Subject = "Email de contato da loja";
+$mail->msgHTML("<html>de: {$nome}<br/>email: {$email}<br/>mensagem: {$mensagem}</html>");
+$mail->AltBody = "de: {$nome}\nemail:{$email}\nmensagem: {$mensagem}";
+
+if($mail->send()) {
+    $_SESSION["success"] = "Mensagem enviada com sucesso";
+    header("Location: index.php");
+} else {
+    $_SESSION["danger"] = "Erro ao enviar mensagem " . $mail->ErrorInfo;
+    header("Location: contato.php");
+}
+die();
 
 ?>
